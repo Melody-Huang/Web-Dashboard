68988786ded7ef08427772a7238094a6eda6d16b
Melody <wrb302@berkeley.edu>
2024-08-08 16:36:13 -0700

    Make the first version of dashboard homepage and detail pages.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white shadow-lg">
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="flex justify-between">
+          <div className="flex space-x-7">
+            <div>
+              <Link to="/" className="flex items-center py-4 px-2">
+                <span className="font-semibold text-gray-500 text-lg">Crypto Dashboard</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
