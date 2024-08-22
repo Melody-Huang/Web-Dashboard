46f04c9dcdfdb79bbcad7a2e7d767e695b8c983e
Melody <wrb302@berkeley.edu>
2024-08-22 11:05:21 -0700

    hardcode api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 // src/api.js
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3003';
+// const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3003';
+const baseURL = 'https://crypto-dashboard-api.melody-hch.workers.dev/';
 
 const api = axios.create({
   baseURL: baseURL,
